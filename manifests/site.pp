6110cf6e811e23882b261edd76c5cc1526ae2074
dpasechnik2tests <dpasechnik-git-tests@iplogic.com.ua>
2016-02-18 21:30:12 +0000

    testingtemplates added site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,23 +1,30 @@
+class base {
+#  include sudo
+#  include ssh
+}
+
 node '755f43a83f6b' 
 {
-
+	
 }
 
 #db.example.com
 node 'c2a5a15b507a' 
 {
-
+  include base
+  include apache
 }
 
 
 #mail.example.com
 node '5682d605f3ea' 
 {
-
+  include base
+  include testingtemplates
 }
 
 #web.example.com
 node 'd0529c638461'
 {
-
+  include base
 }
